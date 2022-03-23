161625de4de9fa6218eeb02901f3164f09cd60c8
rzvr <rmzvr@protonmail.com>
2022-03-23 23:49:26 +0200

    Updated global styles

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import App from './components/App'
 import { store } from './store'
 import { Provider } from 'react-redux'
 
+// global styles
+import styles from './index.module.scss'
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
